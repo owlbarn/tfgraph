50f173bcaf458ba30e407ad03aa8569142f22946
jzstark <rho.ajax@gmail.com>
2019-07-18 22:10:03 +0100

    updating serialisation func

--- bin/serialise.py
+++ bin/serialise.py
@@ -10,7 +10,11 @@ variable.initializer_name = sys.argv[3]
 variable.snapshot_name = sys.argv[4]
 variable.is_resource = False
 variable.trainable = True
-variable.synchronization = 0
-variable.aggregation = 0
+#variable.synchronization = 0
+#variable.aggregation = 0
+
+#save_variable = variable.save_slice_info_def
+#save_variable.full_name = "shit"
+#save_variable.full_shape.extend([1,2,3])
 
 print(variable.SerializeToString())
